a71f644ef9cc0f30b0a1bcc86851c5731370dfb1
Andreas Norman <andreas.norman@knowit.se>
2014-11-18 14:48:58 +0100

    no message

--- src/jquery.simplesharebuttons.js
+++ src/jquery.simplesharebuttons.js
@@ -10,6 +10,11 @@
 		// References to DOM and jQuery versions of element.
 		var el = element;
 		var $el = $(element);
+		var totalCount = 0;
+		var twitterCount = 0;
+		var facebookCount = 0;
+		var linkedinCount = 0;
+		var googleplusCount = 0;
 
 		// Extend default options with those supplied by user.
 		options = $.extend({}, $.fn[pluginName].defaults, options);
@@ -23,226 +28,268 @@
 		}
 
 		function attachEvents() {
+			$el.find('.sharebutton').each(function (i) {
+				if ($(this).data('sharetype') === 'twitter') {
+					attachTwitter(this);
+					if (options.fetchCounts) {
+						fetchTwitterCount(this);
+					}
+				} else if ($(this).data('sharetype') === 'facebook') {
+					attachFacebook(this);
+					if (options.fetchCounts) {
+						fetchFacebookCount(this);
+					}
+				} else if ($(this).data('sharetype') === 'linkedin') {
+					attachLinkedIn(this);
+					if (options.fetchCounts) {
+						fetchLinkedInCount(this);
+					}
+				} else if ($(this).data('sharetype') === 'googleplus') {
+					attachGooglePlus(this);
+					if (options.fetchCounts) {
+						fetchGooglePlusCount(this);
+					}
+				}
+				afterLoad();
+			});
+		}
+
+		function afterLoad() {
+			$(document).ajaxStop(function () {
+				hook('onLoad');
+			});
+		}
+
+		function fetchFacebookCount(element) {
+			/*
+			http://graph.facebook.com/?id=http://{URL}
+			{
+			"id": "http://{URL}",
+			"shares": intgr/(number)
+			}
+			*/
+			$.ajax({
+				url: 'http://graph.facebook.com/?id=' + getButtonURL(element),
+				async: true,
+				dataType: 'json',
+			}).done(function (response) {
 				var count = 0;
-				$el.find('.sharebutton').each(function (i) {
-					if ($(this).data('sharetype') === 'twitter') {
-						attachTwitter(this);
-						if (options.fetchCounts) {
-							fetchTwitterCount(getButtonURL(this));
-							$('.twitter-socialcount').html(count);
-						}
-					} else if ($(this).data('sharetype') === 'facebook') {
-						attachFacebook(this);
-						if (options.fetchCounts) {
-							fetchFacebookCount(getButtonURL(this));
-						}
-					} else if ($(this).data('sharetype') === 'linkedin') {
-						attachLinkedIn(this);
-						if (options.fetchCounts) {
-							fetchLinkedInCount(getButtonURL(this));
-						}
-					} else if ($(this).data('sharetype') === 'googleplus') {
-						attachGooglePlus(this);
-						if (options.fetchCounts) {
-							fetchGooglePlusCount(getButtonURL(this));
-						}
-					}
-					afterLoad();
-				});
-			}
-
-			function afterLoad() {
-				$( document ).ajaxStop(function() {
-					hook('onLoad');
-				});
-			}
-
-			function fetchFacebookCount(url) {
-				/*
-				http://graph.facebook.com/?id=http://{URL}
-				{
-				"id": "http://{URL}",
-				"shares": intgr/(number)
-				}
-				*/
-				$.ajax({
-					url: 'http://graph.facebook.com/?id=' + url,
-					async: true,
-					dataType: 'json',
-				}).done(function (response) {
-					if (typeof response.shares !== 'undefined') {
-						$('.facebook-socialcount').html(response.shares);
-					} else {
-						$('.facebook-socialcount').html(0);
-					}
-
-				});
-			}
-
-			function fetchTwitterCount(url) {
-				/*
-				http://cdn.api.twitter.com/1/urls/count.json?url=http://{URL}
-				{
-				"count": intgr/(number)
-				"url":"http:\/\/{URL}\/"
-				}
-				*/
-				$.ajax({
-					url: 'http://cdn.api.twitter.com/1/urls/count.json?url=' + url + '&callback=?',
-					async: true,
-					dataType: 'json',
-				}).done(function (response) {
-					$('.twitter-socialcount').html(response.count);
-				});
-			}
-
-			function fetchLinkedInCount(url) {
-				/*
-				http://www.linkedin.com/countserv/count/share?url=http://{URL&format=json
-				{
-				"count": intgr/(number),
-				"fCnt": "intgr/(number)",
-				"fCntPlusOne":"intgr/(number) + 1", // increased by one
-				"url":"http:\/\/{URL}"
-				}
-				*/
-				$.ajax({
-					url: 'http://www.linkedin.com/countserv/count/share?url=' + url + '&callback=?',
-					async: true,
-					dataType: 'json',
-				}).done(function (response) {
-					$('.linkedin-socialcount').html(response.count);
-				});
-			}
-
-			function fetchGooglePlusCount(url) {
-				$.ajax({
-					url: options.GooglePlusAPIProvider + '?url=' + url,
-					async: true,
-					dataType: 'text',
-				}).done(function (response) {
-					$('.googleplus-socialcount').html(response);
-				});
-			}
-
-			function attachFacebook(button) {
-				$(button).on('click', function (e) {
-					e.preventDefault();
-					var url = getButtonURL(this);
-					var fullurl = 'u=' + encodeURIComponent(url);
-					var encodedUrl = encodeURIComponent(url);
-					window.facebook = window.facebook || {};
-					window.facebook.shareWin = window.open('https://www.facebook.com/sharer/sharer.php?' + fullurl, '', getWindowSizePosition());
-					return false;
-				});
-			}
-
-			function attachLinkedIn(button) {
-				$(button).on('click', function (e) {
-					e.preventDefault();
-					var original_referer = getButtonReferer(this);
-					var url = getButtonURL(this);
-					var fullurl = 'original_referer=' + encodeURIComponent(original_referer) + '&url=' + encodeURIComponent(url);
-					var encodedUrl = encodeURIComponent(url);
-					window.GooglePlus = window.GooglePlus || {};
-					window.GooglePlus.shareWin = window.open('https://www.linkedin.com/cws/share?' + fullurl + '&isFramed=true', '', getWindowSizePosition());
-					return false;
-				});
-			}
-
-			function attachReddit(button) {
-				$(button).on('click', function (e) {
-					e.preventDefault();
-					var url = getButtonURL(this);
-					var fullurl = 'url=' + encodeURIComponent(url);
-					var encodedUrl = encodeURIComponent(url);
-					window.Reddit = window.Reddit || {};
-					window.Reddit.shareWin = window.open('http://www.reddit.com/submit?' + fullurl, '', getWindowSizePosition());
-					return false;
-				});
-			}
-
-			function attachGooglePlus(button) {
-				$(button).on('click', function (e) {
-					e.preventDefault();
-					var text = getButtonText(this);
-					var url = getButtonURL(this);
-					var fullurl = 'text=' + encodeURIComponent(text) + '&url=' + encodeURIComponent(url);
-					var encodedUrl = encodeURIComponent(url);
-					window.GooglePlus = window.GooglePlus || {};
-					window.GooglePlus.shareWin = window.open('https://plus.google.com/share?' + fullurl, '', getWindowSizePosition());
-					return false;
-				});
-			}
-
-			function attachTwitter(button) {
-				$(button).on('click', function (e) {
-					e.preventDefault();
-					var text = getButtonText(this);
-					var url = getButtonURL(this);
-					var via = getButtonVia(this);
-					var related = getButtonRelated(this);
-					var fullurl = 'text=' + encodeURIComponent(text) + '&url=' + encodeURIComponent(url) + '&via=' + via + '&related=' + related;
-					var encodedUrl = encodeURIComponent(url);
-					window.Twitter = window.Twitter || {};
-					window.Twitter.shareWin = window.open('https://twitter.com/intent/tweet?' + fullurl, '', getWindowSizePosition());
-					return false;
-				});
-			}
-
-			function getButtonVia (element) {
-				if (typeof $(element).data('via') !== 'undefined') {
-					return $(element).data('via');
+				if (typeof response.shares !== 'undefined') {
+					count = getBaseCount(element) + parseInt(response.shares, 10);
+					$(element).find('.count').html(count);
+					totalCount = totalCount + count;
+					facebookCount = count;
 				} else {
-					return '';
+					count = getBaseCount(element);
+					$(element).find('.count').html(count);
+					totalCount = totalCount + count;
+					facebookCount = count;
 				}
-			}
 
-			function getButtonText (element) {
-				if (typeof $(element).data('text') !== 'undefined') {
-					return $(element).data('text');
-				} else {
-					return '';
-				}
-			}
+			});
+		}
 
-			function getButtonRelated (element) {
-				if (typeof $(element).data('related') !== 'undefined') {
-					return $(element).data('related');
-				} else {
-					return '';
-				}
+		function fetchTwitterCount(element) {
+			/*
+			http://cdn.api.twitter.com/1/urls/count.json?url=http://{URL}
+			{
+			"count": intgr/(number)
+			"url":"http:\/\/{URL}\/"
 			}
+			*/
+			$.ajax({
+				url: 'http://cdn.api.twitter.com/1/urls/count.json?url=' + getButtonURL(element) + '&callback=?',
+				async: true,
+				dataType: 'json',
+			}).done(function (response) {
+				var count = getBaseCount(element) + parseInt(response.count, 10);
+				$(element).find('.count').html(count);
+				totalCount = totalCount + count;
+				twitterCount = count;
+			});
+		}
 
-			function getButtonURL (element) {
-				if (typeof $(element).data('url') !== 'undefined') {
-					return $(element).data('url');
-				} else {
-					return document.URL;
-				}
+		function fetchLinkedInCount(element) {
+			/*
+			http://www.linkedin.com/countserv/count/share?url=http://{URL&format=json
+			{
+			"count": intgr/(number),
+			"fCnt": "intgr/(number)",
+			"fCntPlusOne":"intgr/(number) + 1", // increased by one
+			"url":"http:\/\/{URL}"
 			}
+			*/
+			$.ajax({
+				url: 'http://www.linkedin.com/countserv/count/share?url=' + getButtonURL(element) + '&callback=?',
+				async: true,
+				dataType: 'json',
+			}).done(function (response) {
+				var count = getBaseCount(element) + parseInt(response.count, 10);
+				$(element).find('.count').html(count);
+				totalCount = totalCount + count;
+				linkedinCount = count;
+			});
+		}
 
-			function getButtonReferer (element) {
-				if (typeof $(element).data('referer') !== 'undefined') {
-					return $(element).data('referer');
-				} else {
-					return '';
-				}
-			}
+		function fetchGooglePlusCount(element) {
+			$.ajax({
+				url: options.GooglePlusAPIProvider + '?url=' + getButtonURL(element),
+				async: true,
+				dataType: 'text',
+			}).done(function (response) {
+				var count = getBaseCount(element) + parseInt(response, 10);
+				$(element).find('.count').html(count);
+				totalCount = totalCount + count;
+				googleplusCount = count;
+			});
+		}
 
-			function getWindowSizePosition () {
-				var D = 550,
-					A = 335,
-					C = screen.height,
-					B = screen.width,
-					H = Math.round((B / 2) - (D / 2)),
-					G = 0,
-					F = document;
-				if (C > A) {
-					G = Math.round((C / 2) - (A / 2));
-				}
-				return 'left=' + H + ',top=' + G + ',width=' + D + ',height=' + A + ',personalbar=0,toolbar=0,scrollbars=1,resizable=1';
+		function attachFacebook(button) {
+			$(button).on('click', function (e) {
+				e.preventDefault();
+				var url = getButtonURL(this);
+				var fullurl = 'u=' + encodeURIComponent(url);
+				var encodedUrl = encodeURIComponent(url);
+				window.facebook = window.facebook || {};
+				window.facebook.shareWin = window.open('https://www.facebook.com/sharer/sharer.php?' + fullurl, '', getWindowSizePosition());
+				return false;
+			});
+		}
+
+		function attachLinkedIn(button) {
+			$(button).on('click', function (e) {
+				e.preventDefault();
+				var original_referer = getButtonReferer(this);
+				var url = getButtonURL(this);
+				var fullurl = 'original_referer=' + encodeURIComponent(original_referer) + '&url=' + encodeURIComponent(url);
+				var encodedUrl = encodeURIComponent(url);
+				window.GooglePlus = window.GooglePlus || {};
+				window.GooglePlus.shareWin = window.open('https://www.linkedin.com/cws/share?' + fullurl + '&isFramed=true', '', getWindowSizePosition());
+				return false;
+			});
+		}
+
+		function attachReddit(button) {
+			$(button).on('click', function (e) {
+				e.preventDefault();
+				var url = getButtonURL(this);
+				var fullurl = 'url=' + encodeURIComponent(url);
+				var encodedUrl = encodeURIComponent(url);
+				window.Reddit = window.Reddit || {};
+				window.Reddit.shareWin = window.open('http://www.reddit.com/submit?' + fullurl, '', getWindowSizePosition());
+				return false;
+			});
+		}
+
+		function attachGooglePlus(button) {
+			$(button).on('click', function (e) {
+				e.preventDefault();
+				var text = getButtonText(this);
+				var url = getButtonURL(this);
+				var fullurl = 'text=' + encodeURIComponent(text) + '&url=' + encodeURIComponent(url);
+				var encodedUrl = encodeURIComponent(url);
+				window.GooglePlus = window.GooglePlus || {};
+				window.GooglePlus.shareWin = window.open('https://plus.google.com/share?' + fullurl, '', getWindowSizePosition());
+				return false;
+			});
+		}
+
+		function attachTwitter(button) {
+			$(button).on('click', function (e) {
+				e.preventDefault();
+				var text = getButtonText(this);
+				var url = getButtonURL(this);
+				var via = getButtonVia(this);
+				var related = getButtonRelated(this);
+				var fullurl = 'text=' + encodeURIComponent(text) + '&url=' + encodeURIComponent(url) + '&via=' + via + '&related=' + related;
+				var encodedUrl = encodeURIComponent(url);
+				window.Twitter = window.Twitter || {};
+				window.Twitter.shareWin = window.open('https://twitter.com/intent/tweet?' + fullurl, '', getWindowSizePosition());
+				return false;
+			});
+		}
+
+		function getButtonVia(element) {
+			if (typeof $(element).data('via') !== 'undefined') {
+				return $(element).data('via');
+			} else {
+				return '';
 			}
+		}
+
+		function getButtonText(element) {
+			if (typeof $(element).data('text') !== 'undefined') {
+				return $(element).data('text');
+			} else {
+				return '';
+			}
+		}
+
+		function getButtonRelated(element) {
+			if (typeof $(element).data('related') !== 'undefined') {
+				return $(element).data('related');
+			} else {
+				return '';
+			}
+		}
+
+		function getButtonURL(element) {
+			if (typeof $(element).data('url') !== 'undefined') {
+				return $(element).data('url');
+			} else {
+				return document.URL;
+			}
+		}
+
+		function getButtonReferer(element) {
+			if (typeof $(element).data('referer') !== 'undefined') {
+				return $(element).data('referer');
+			} else {
+				return '';
+			}
+		}
+
+		function getBaseCount(element) {
+			if (typeof $(element).data('basecount') !== 'undefined') {
+				return $(element).data('basecount');
+			} else {
+				return 0;
+			}
+		}
+
+		function getWindowSizePosition() {
+			var D = 550,
+				A = 335,
+				C = screen.height,
+				B = screen.width,
+				H = Math.round((B / 2) - (D / 2)),
+				G = 0,
+				F = document;
+			if (C > A) {
+				G = Math.round((C / 2) - (A / 2));
+			}
+			return 'left=' + H + ',top=' + G + ',width=' + D + ',height=' + A + ',personalbar=0,toolbar=0,scrollbars=1,resizable=1';
+		}
+
+		function getTotalCount() {
+			return totalCount;
+		}
+
+		function getFacebookCount() {
+			return facebookCount;
+		}
+
+		function getTwitterCount() {
+			return twitterCount;
+		}
+
+		function getLinkedinCount() {
+			return linkedinCount;
+		}
+
+		function getGooglePlusCount() {
+			return googleplusCount;
+		}
 
 		/**
 		 * Get/set a plugin option.
@@ -296,7 +343,12 @@
 		// Expose methods of Plugin we wish to be public.
 		return {
 			option: option,
-			destroy: destroy
+			destroy: destroy,
+			getTotalCount: getTotalCount,
+			getFacebookCount: getFacebookCount,
+			getGooglePlusCount: getGooglePlusCount,
+			getLinkedinCount: getLinkedinCount,
+			getTwitterCount: getTwitterCount
 		};
 	}
 
